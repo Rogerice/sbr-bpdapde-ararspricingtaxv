c8f1b7ea4a6bb83305bf2372b947573ad344109c
Rogerio da Silva Rodrigues (P) <rogerio.rodrigues@prservicos.com.br>
2024-12-04 08:31:24 -0300

    refatorando a classe de acordo com o apontamento do Sonar

--- src/main/java/com/santander/bp/service/OffersPricingServiceBP82.java
+++ src/main/java/com/santander/bp/service/OffersPricingServiceBP82.java
@@ -27,130 +27,119 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class OffersPricingServiceBP82 {
 
-  @Autowired private OffersMapperBP82 offersMapperBP82;
+    @Autowired private OffersMapperBP82 offersMapperBP82;
 
-  @Autowired private ArsenalAltairConfig arsenalAltairConfig;
+    @Autowired private ArsenalAltairConfig arsenalAltairConfig;
 
-  @Autowired private AltairStrategy altairStrategy;
+    @Autowired private AltairStrategy altairStrategy;
 
-  @Autowired private AltairFacade altairFacade;
+    @Autowired private AltairFacade altairFacade;
 
-  public List<OffersPricingResponse> processOffers(OffersPricingRequest offersPricingRequest)
-      throws AltairException {
+    public List<OffersPricingResponse> processOffers(OffersPricingRequest offersPricingRequest)
+        throws AltairException {
 
-    log.info(
-        "Iniciando o mapeamento da requisição para BPMP82. Dados recebidos: {}",
-        offersPricingRequest);
+        log.info(
+            "Iniciando o mapeamento da requisição para BPMP82. Dados recebidos: {}",
+            offersPricingRequest);
 
-    List<OffersPricingResponse> responseList = new ArrayList<>();
-    boolean recall;
+        List<OffersPricingResponse> responseList = new ArrayList<>();
+        boolean recall;
 
-    do {
-      BPMP82 bpmp82 = offersMapperBP82.mapOffersRequest(offersPricingRequest);
-      log.info("Dados mapeados para BPMP82: {}", bpmp82);
+        do {
+            BPMP82 bpmp82 = offersMapperBP82.mapOffersRequest(offersPricingRequest);
+            log.info("Dados mapeados para BPMP82: {}", bpmp82);
 
-      ResponseDto altairReturn = sendMessageAltair(Transaction.BP82, bpmp82);
-      log.info("Resposta do Altair recebida: {}", altairReturn);
+            ResponseDto altairReturn = sendMessageAltair(Transaction.BP82, bpmp82);
+            log.info("Resposta do Altair recebida: {}", altairReturn);
 
-      handleErrors(altairReturn);
-      responseList.addAll(offersMapperBP82.mapOffersResponseList(altairReturn));
+            handleErrors(altairReturn);
+            responseList.addAll(offersMapperBP82.mapOffersResponseList(altairReturn));
 
-      recall = shouldRecall(altairReturn, offersPricingRequest);
+            recall = shouldRecall(altairReturn, offersPricingRequest);
 
-    } while (recall);
+        } while (recall);
 
-    return responseList;
-  }
-
-  private boolean shouldRecall(
-      ResponseDto altairReturn, OffersPricingRequest offersPricingRequest) {
-    if (!altairReturn.getObjeto().getListaFormatos().isEmpty()) {
-      var lastFormat =
-          altairReturn
-              .getObjeto()
-              .getListaFormatos()
-              .get(altairReturn.getObjeto().getListaFormatos().size() - 1);
-
-      // Primeiro verificamos se o lastFormat é do tipo PsScreen
-      if (lastFormat instanceof PsScreen) {
-        PsScreen psScreen = (PsScreen) lastFormat;
-
-        // Depois verificamos se o formato do PsScreen é do tipo BPMP82
-        if (psScreen.getFormato() instanceof BPMP82) {
-          BPMP82 bpmp82Response = (BPMP82) psScreen.getFormato();
-
-          // Validação do valor "S" utilizando o enum
-          String indicatorRecall = bpmp82Response.getINDREA();
-          if (FixedFieldsEnum.RECALL_INDICATOR.getValue().equals(indicatorRecall)) {
-            // Atualiza o request com os dados para a próxima iteração
-            offersPricingRequest.setIndicatorRecall(bpmp82Response.getINDREA());
-            offersPricingRequest.setCdRecall(bpmp82Response.getPRODREA());
-            offersPricingRequest.setSubProductRecall(bpmp82Response.getSUBPREA());
-
-            return true;
-          }
-        }
-      }
+        return responseList;
     }
-    return false;
-  }
 
-  private void handleErrors(ResponseDto responseDto) {
-    if (responseDto.getObjeto().getListaErros() != null
-        && !responseDto.getObjeto().getListaErros().isEmpty()) {
-      log.warn("Erro retornado pela alta plataforma.");
-      responseDto
-          .getObjeto()
-          .getListaErros()
-          .forEach(
-              erro -> {
-                if (erro instanceof PsError) {
-                  PsError psError = (PsError) erro;
-                  log.warn(
-                      "Código do Erro: {}, Mensagem do Erro: {}",
-                      psError.getCodigo(),
-                      psError.getMensagem());
+    private boolean shouldRecall(ResponseDto altairReturn, OffersPricingRequest offersPricingRequest) {
+        if (!altairReturn.getObjeto().getListaFormatos().isEmpty()) {
+            var lastFormat =
+                altairReturn
+                    .getObjeto()
+                    .getListaFormatos()
+                    .get(altairReturn.getObjeto().getListaFormatos().size() - 1);
+
+            if (lastFormat instanceof PsScreen psScreen && psScreen.getFormato() instanceof BPMP82 bpmp82Response) {
+
+                String indicatorRecall = bpmp82Response.getINDREA();
+                if (FixedFieldsEnum.RECALL_INDICATOR.getValue().equals(indicatorRecall)) {
+                    offersPricingRequest.setIndicatorRecall(bpmp82Response.getINDREA());
+                    offersPricingRequest.setCdRecall(bpmp82Response.getPRODREA());
+                    offersPricingRequest.setSubProductRecall(bpmp82Response.getSUBPREA());
+
+                    return true;
                 }
-              });
-      throw new AltairException(
-          "ERRO_ALTAPLATAFORMA", "Erro na transação", "Detalhes dos erros encontrados");
+            }
+        }
+        return false;
     }
-  }
 
-  private ResponseDto sendMessageAltair(Transaction transaction, Object dados) {
-    try {
-      log.info(
-          "Iniciando envio para Altair MQ. Transação: {}, Dados: {}", transaction.name(), dados);
-
-      configurarArsenalConfig(transaction);
-      altairFacade = altairStrategy.getAltairFacade(arsenalAltairConfig, dados);
-
-      Instant startingPoint = Instant.now();
-      ResponseDto saida = altairFacade.executeIntegrationAltair();
-
-      logResponseTime(startingPoint);
-      log.info("Resposta recebida do Altair MQ: {}", saida);
-
-      return saida;
-
-    } catch (Exception e) {
-      log.error(
-          "Erro ao enviar mensagem para Altair MQ. Transação: {}, Dados: {}, Erro: {}",
-          transaction.name(),
-          dados,
-          e.getMessage(),
-          e);
-      throw new AltairException(
-          "MQ_ERROR", e.getMessage(), "Erro ao enviar mensagem para o Altair MQ");
+    private void handleErrors(ResponseDto responseDto) {
+        if (responseDto.getObjeto().getListaErros() != null
+            && !responseDto.getObjeto().getListaErros().isEmpty()) {
+            log.warn("Erro retornado pela alta plataforma.");
+            responseDto
+                .getObjeto()
+                .getListaErros()
+                .forEach(
+                    erro -> {
+                        if (erro instanceof PsError psError) {
+                            log.warn(
+                                "Código do Erro: {}, Mensagem do Erro: {}",
+                                psError.getCodigo(),
+                                psError.getMensagem());
+                        }
+                    });
+            throw new AltairException(
+                "ERRO_ALTAPLATAFORMA", "Erro na transação", "Detalhes dos erros encontrados");
+        }
     }
-  }
 
-  private void configurarArsenalConfig(Transaction transaction) {
-    arsenalAltairConfig.setTransactionName(transaction.name());
-  }
+    private ResponseDto sendMessageAltair(Transaction transaction, Object dados) {
+        try {
+            log.info(
+                "Iniciando envio para Altair MQ. Transação: {}, Dados: {}", transaction.name(), dados);
 
-  private void logResponseTime(Instant startingPoint) {
-    long responseTime = Duration.between(startingPoint, Instant.now()).toMillis();
-    log.info("MQ response time {} ms", responseTime);
-  }
+            configurarArsenalConfig(transaction);
+            altairFacade = altairStrategy.getAltairFacade(arsenalAltairConfig, dados);
+
+            Instant startingPoint = Instant.now();
+            ResponseDto saida = altairFacade.executeIntegrationAltair();
+
+            logResponseTime(startingPoint);
+            log.info("Resposta recebida do Altair MQ: {}", saida);
+
+            return saida;
+
+        } catch (Exception e) {
+            log.error(
+                "Erro ao enviar mensagem para Altair MQ. Transação: {}, Dados: {}, Erro: {}",
+                transaction.name(),
+                dados,
+                e.getMessage(),
+                e);
+            throw new AltairException(
+                "MQ_ERROR", e.getMessage(), "Erro ao enviar mensagem para o Altair MQ");
+        }
+    }
+
+    private void configurarArsenalConfig(Transaction transaction) {
+        arsenalAltairConfig.setTransactionName(transaction.name());
+    }
+
+    private void logResponseTime(Instant startingPoint) {
+        long responseTime = Duration.between(startingPoint, Instant.now()).toMillis();
+        log.info("MQ response time {} ms", responseTime);
+    }
 }
