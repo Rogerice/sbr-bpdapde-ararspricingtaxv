c3860dbdb72009092341b69728180a4ee2a2463d
Rogério da Silva Rodrigues <x100049@prservicos.com.br>
2025-06-11 15:44:39 -0300

    ajustando codigo para passar no SONAR

--- src/main/java/com/santander/bp/app/mapper/PricingRequestMapper.java
+++ src/main/java/com/santander/bp/app/mapper/PricingRequestMapper.java
@@ -1,7 +1,12 @@
 package com.santander.bp.app.mapper;
 
 import com.santander.bp.model.OffersPricingResponse;
+import com.santander.bp.model.external.BenchmarkIndex;
+import com.santander.bp.model.external.InvestmentOrder;
+import com.santander.bp.model.external.InvestmentPricingCondition;
 import com.santander.bp.model.external.PricingRequest;
+import com.santander.bp.model.external.Product;
+import com.santander.bp.model.external.PromotionalCode;
 import java.util.List;
 import lombok.Generated;
 import org.springframework.stereotype.Component;
@@ -11,9 +16,34 @@ import org.springframework.stereotype.Component;
 public class PricingRequestMapper {
 
   public PricingRequest buildFromCosmosOffers(List<OffersPricingResponse> cosmosOffers) {
-    // construção do pricing
+    if (cosmosOffers == null || cosmosOffers.isEmpty()) {
+      return new PricingRequest();
+    }
+
+    OffersPricingResponse offer = cosmosOffers.get(0);
+
+    InvestmentOrder investmentOrder = new InvestmentOrder();
+    investmentOrder.setTypeCode("STANDARD");
+
+    PromotionalCode promo = new PromotionalCode();
+    investmentOrder.setPromotionalCode(promo);
+
+    InvestmentPricingCondition condition = new InvestmentPricingCondition();
+
+    Product product = new Product();
+    product.setName(offer.getSubProduct());
+    condition.setProduct(product);
+
+    BenchmarkIndex benchmark = new BenchmarkIndex();
+    condition.setBenchmarkIndex(benchmark);
+
+    PromotionalCode promoCond = new PromotionalCode();
+    condition.setPromotionalCode(promoCond);
+
     PricingRequest request = new PricingRequest();
-    // preenche os campos com base no cosmosOffers, e pricing ...
+    request.setInvestmentOrder(investmentOrder);
+    request.setInvestmentPricingConditions(List.of(condition));
+
     return request;
   }
 }
